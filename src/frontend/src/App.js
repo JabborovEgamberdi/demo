fb67695877dd703d93e58971010ffa89302f88e0
EgamberdiJabborov <jabborovegamberdi2002@gmail.com>
2023-11-30 18:34:29 +0500

    Updated

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,14 +1,10 @@
 import {useEffect, useState} from "react";
 import {getAllStudents} from "./Client";
-import {Breadcrumb, Layout, Menu} from "antd";
+import {Breadcrumb, Layout, Menu, Table, Spin, Empty} from "antd";
+
 import './App.css';
-import {
-    DesktopOutlined,
-    FileOutlined,
-    PieChartOutlined,
-    TeamOutlined,
-    UserOutlined,
-} from '@ant-design/icons';
+import {DesktopOutlined, FileOutlined, PieChartOutlined, TeamOutlined, UserOutlined,} from '@ant-design/icons';
+
 const {
     Header,
     Content,
@@ -17,12 +13,34 @@ const {
 } = Layout;
 const {SubMenu} = Menu;
 
-
+const columns = [
+    {
+        title: 'Id',
+        dataIndex: 'id',
+        key: 'id',
+    },
+    {
+        title: 'Name',
+        dataIndex: 'name',
+        key: 'name',
+    },
+    {
+        title: 'Email',
+        dataIndex: 'email',
+        key: 'email',
+    },
+    {
+        title: 'Gender',
+        dataIndex: 'gender',
+        key: 'gender',
+    },
+];
 
 function App() {
 
     const [students, setStudents] = useState([]);
     const [collapsed, setCollapsed] = useState(false);
+    const [fetching, setFetching] = useState(true);
 
     const fetchStudents = () =>
         getAllStudents()
@@ -31,6 +49,9 @@ function App() {
                 console.log(data);
                 setStudents(data);
             })
+            .finally(() => {
+                setFetching(false)
+            })
 
     useEffect(() => {
         console.log("component is mounted");
@@ -38,64 +59,66 @@ function App() {
             .then();
     }, []);
 
-    if (students.length <= 0) {
-        return "no data";
+    const renderStudents = () => {
+
+        if (fetching) {
+            return <Spin/>
+        }
+
+        if (students == null || students.length <= 0) {
+            return <Empty/>;
+        }
+
+        return <Table
+            dataSource={students}
+            columns={columns}
+            bordered
+            title={() => 'Students'}
+            pagination={{pageSize: 10}}
+            scroll={{y: 350}}
+            rowKey={(student) => student.id}
+        />;
     }
 
     return <Layout style={{minHeight: '100vh'}}>
         <Sider collapsible collapsed={collapsed}
                onCollapse={setCollapsed}>
-            <div className="logo" />
+            <div className="logo"/>
             <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
-                <Menu.Item key="1" icon={<PieChartOutlined />}>
+                <Menu.Item key="1" icon={<PieChartOutlined/>}>
                     Option 1
                 </Menu.Item>
-                <Menu.Item key="2" icon={<DesktopOutlined />}>
+                <Menu.Item key="2" icon={<DesktopOutlined/>}>
                     Option 2
                 </Menu.Item>
-                <SubMenu key="sub1" icon={<UserOutlined />} title="User">
+                <SubMenu key="sub1" icon={<UserOutlined/>} title="User">
                     <Menu.Item key="3">Tom</Menu.Item>
                     <Menu.Item key="4">Bill</Menu.Item>
                     <Menu.Item key="5">Alex</Menu.Item>
                 </SubMenu>
-                <SubMenu key="sub2" icon={<TeamOutlined />} title="Team">
+                <SubMenu key="sub2" icon={<TeamOutlined/>} title="Team">
                     <Menu.Item key="6">Team 1</Menu.Item>
                     <Menu.Item key="8">Team 2</Menu.Item>
                 </SubMenu>
-                <Menu.Item key="9" icon={<FileOutlined />}>
+                <Menu.Item key="9" icon={<FileOutlined/>}>
                     Files
                 </Menu.Item>
             </Menu>
         </Sider>
 
         <Layout className="site-layout">
-            <Header className="site-layout-background"
-                    style={{
-                        padding: 0,
-                        // background: colorBgContainer,
-                    }}
+            <Header className="site-layout-background" style={{
+                padding: 0,
+                // background: colorBgContainer,
+            }}
             />
-            <Content
-                style={{
-                    margin: '0 16px',
-                }}
-            >
-                <Breadcrumb
-                    style={{
-                        margin: '16px 0',
-                    }}
-                >
+            <Content style={{margin: '0 16px'}}>
+                <Breadcrumb style={{margin: '16px 0'}}>
                     <Breadcrumb.Item>User</Breadcrumb.Item>
                     <Breadcrumb.Item>Bill</Breadcrumb.Item>
                 </Breadcrumb>
-                <div
-                    style={{
-                        padding: 24,
-                        minHeight: 360,
-                        // background: colorBgContainer,
-                    }}
-                >
-                    Bill is a cat.
+                <div className="site-layout-background" style={{padding: 24, minHeight: 360}}>
+                    {renderStudents()}
                 </div>
             </Content>
             <Footer style={{textAlign: 'center'}}>
